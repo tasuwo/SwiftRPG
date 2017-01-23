92cdec79605762447b0275422d98228abcb89037
tasuwo <kamuhata.you@gmail.com>
2017-01-23 21:49:00 +0900

    Clean code

--- SwiftRPG/src/Model/Map/TileSheet/TileSheet.swift
+++ SwiftRPG/src/Model/Map/TileSheet/TileSheet.swift
@@ -10,6 +10,7 @@ import Foundation
 import UIKit
 import SpriteKit
 import SwiftyJSON
+
 // FIXME: comparison operators with optionals were removed from the Swift Standard Libary.
 // Consider refactoring the code to use the non-optional operators.
 fileprivate func < <T : Comparable>(lhs: T?, rhs: T?) -> Bool {
