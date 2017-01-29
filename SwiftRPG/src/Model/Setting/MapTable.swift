69b6fbc4d44e270fb66350225c7736a892cc893c
tasuwo <kamuhata.you@gmail.com>
2017-01-30 00:58:59 +0900

    Add new game scene

--- SwiftRPG/src/Model/Setting/MapTable.swift
+++ SwiftRPG/src/Model/Setting/MapTable.swift
@@ -11,6 +11,6 @@ import Foundation
 struct MapTable {
     static let fromJsonFileName: Dictionary<String, GameScene.Type> = [
         "sample_map02.json": myGameScene.self,
-        // "sample_map01.json": nextGameScene.self
+        "sample_map01.json": nextGameScene.self
     ]
 }
---
+++ SwiftRPG/src/View/components/nextGameScene.swift
@@ -0,0 +1,57 @@
+//
+//  nextGameScene.swift
+//  SwiftRPG
+//
+//  Created by tasuku tozawa on 2017/01/29.
+//  Copyright © 2017年 兎澤佑. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+import UIKit
+
+class nextGameScene: GameScene {
+    required init(size: CGSize, playerCoordiante: TileCoordinate, playerDirection: DIRECTION) {
+        super.init(size: size, playerCoordiante: playerCoordiante, playerDirection: playerDirection)
+        Bundle.main.loadNibNamed("nextGameScene", owner: self, options: nil)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func didMove(to view: SKView) {
+        if let map = Map(mapName: "sample_map01", frameWidth: self.frame.width, frameHeight: self.frame.height) {
+            self.map = map
+            self.map!.addSheetTo(self)
+        } else {
+            return
+        }
+
+        let playerInitialPosition = TileCoordinate.getSheetCoordinateFromTileCoordinate(self.playerInitialCoordinate!)
+        let player = Object(name: objectNameTable.PLAYER_NAME,
+                            imageName: objectNameTable.getImageBy(direction: self.playerInitialDirection!),
+                            position: playerInitialPosition,
+                            images: objectNameTable.PLAYER_IMAGE_SET)
+        player.setCollision()
+        self.map!.setObject(player)
+        self.startBehaviors()
+
+        self.map?.sheet?.centerOn(point: player.position, frameWidth: self.frame.width, frameHeight: self.frame.height)
+
+        actionButton.layer.borderColor = UIColor.white.cgColor
+        actionButton.addTarget(self, action: #selector(GameScene.actionButtonTouched(_:)), for: .touchUpInside)
+        actionButton.isHidden = true
+
+        textBox = Dialog(frame_width: self.frame.width, frame_height: self.frame.height)
+        textBox.hide()
+        textBox.setPositionY(Dialog.POSITION.top)
+        textBox.addTo(self)
+
+        eventDialog.isHidden = true
+        eventDialog.layer.backgroundColor = UIColor.black.cgColor
+        eventDialog.layer.borderColor = UIColor.white.cgColor
+
+        menuButton.layer.borderColor = UIColor.white.cgColor
+    }
+}
